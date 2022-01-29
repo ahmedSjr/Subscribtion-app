f5452b9121da25ea8c804d8776c74b2b785e6478
Ahmed Sirag <58979522+ahmedSjr@users.noreply.github.com>
2022-01-29 20:11:23 +0200

    Add subscriber model with factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Database\Seeders;
+use App\Models\Subscriber;
 
 use Illuminate\Database\Seeder;
 
@@ -13,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Subscriber::factory(10)->create();
         // \App\Models\User::factory(10)->create();
     }
 }
